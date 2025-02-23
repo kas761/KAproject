d34a211f316d893104c0f4a6cffae70b09c84cd7
Krzysztof <karci@softserveinc.com>
2025-02-23 14:37:17 +0100

    ci.yml update

---
+++ app.py
@@ -0,0 +1,39 @@
+import argparse
+from logger import Logger
+from data_retrieval import DataRetrieval
+from data_processor import DataProcessor
+
+data_types = ["posts", "comments", "albums", "photos", "todos", "users"]
+url_base = "https://jsonplaceholder.typicode.com/"
+log_file = "process_log.txt"
+
+def main(args):
+    logger = Logger(log_file)
+
+    if args.data_type:
+        data_type = args.data_type.strip().lower()
+    else:
+        while True:
+            data_type = input("Enter the data type - posts, comments, albums, photos, todos, users (esc to exit): ").strip().lower()
+            if data_type == 'esc':
+                print("Exiting the program.")
+                return
+            if data_type in data_types:
+                break
+            else:
+                logger.log(f"Invalid {data_type} data type. Please enter a valid data type.")
+
+    url = f"{url_base}/{data_type}"
+    data_file = f"data/{data_type}.txt"
+    logger.log(f"Processing {data_type} data")
+    processor = DataProcessor(data_file, logger)
+    retriever = DataRetrieval(data_file, data_type, url, processor, logger)
+    content, retrieved_data = retriever.retrieve_data()
+    retriever.write_data(content, retrieved_data)
+    processor.read_file()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Process some data types.")
+    parser.add_argument('--data_type', type=str, help='The type of data to process (posts, comments, albums, photos, todos, users)')
+    args = parser.parse_args()
+    main(args)
---
+++ dockerfile
@@ -0,0 +1,23 @@
+# Use an official Python runtime as a parent image
+FROM python:3.9-slim
+
+# Set the working directory in the container
+WORKDIR /app
+
+# Copy the current directory contents into the container at /app
+COPY . /app
+
+# Install Poetry
+RUN pip install poetry
+
+# Install dependencies
+RUN poetry install --no-root
+
+# Make port 80 available to the world outside this container
+EXPOSE 80
+
+# Define environment variable
+ENV NAME KARC
+
+# Run the CLI when the container launches
+CMD ["poetry", "run", "python", "app.py"]
