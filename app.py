8339eebe85a0c8ee86e9079867b2935b5ba621b7
Krzysztof <karci@softserveinc.com>
2025-02-23 13:33:56 +0100

    0.18

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 import click
+from main import main
 
 @click.command()
-@click.option('--name', default='World', help='Name to greet')
-def greet(name):
-    """Simple program that greets NAME."""
-    click.echo(f'Hello, {name}!')
+@click.option('--url_base', default='https://jsonplaceholder.typicode.com/', help='Base URL for the API')
+@click.option('--data_type', type=click.Choice(['posts', 'comments', 'albums', 'photos', 'todos', 'users']), help='The type of data to process')
+
+def cli(url_base, data_type):
+    args = {'url_base': url_base, 'data_type': data_type}
+    main(args)
 
 if __name__ == '__main__':
-    greet()
+    cli()
